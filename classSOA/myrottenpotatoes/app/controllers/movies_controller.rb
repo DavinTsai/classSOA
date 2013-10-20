0190b4a998849533ba4adcd30dcbadb46542caab
SOA2013-102078507 <hohi1019@gmail.com>
2013-10-20 10:56:03 -0700

    10/21_Ch4.6-4.8,5.1

--- classSOA/myrottenpotatoes/app/controllers/movies_controller.rb
+++ classSOA/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
+ 
   def index
     @movies = Movie.all
   end
@@ -8,6 +9,45 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+	#default: render 'new't template
+    @movie = Movie.new
+  end 
+
+# replaces the 'create' method in controller:
+  def create
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render 'new' # note, 'new' template can access @movie's field values!
+    end
+  end
+
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  # replaces the 'update' method in controller:
+  def update
+    @movie = Movie.find params[:id]
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    else
+      render 'edit' # note, 'edit' template can access @movie's field values!
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 
 end
